89abc9651d68dfe35987d917a54f5a85dbab7e81
Wxffel <discordisWxffel#1639@pm.me>
2021-12-01 21:12:14 +0100

    Day1 solution

---
+++ src/main/kotlin/de/kpaw/solutions/Day1.kt
@@ -0,0 +1,26 @@
+package de.kpaw.solutions
+
+import de.kpaw.Day
+
+fun main() = Day01.run()
+
+object Day01 : Day(1) {
+    override fun part1() =
+        inputLinesAsInts.filterIndexed { index, i -> index != 0 && i > inputLinesAsInts[index - 1] }.size
+
+    override fun part2() =
+        inputLinesAsInts.filterIndexed { index, _ ->
+            index < inputLinesAsInts.size - 3 &&
+            inputLinesAsInts.slice(index+1..index+3).sum() > inputLinesAsInts.slice(index..index+2).sum()
+        }.size
+}
+
+// Better Solution by BestAuto:
+/*
+// 15
+override fun part1() = input.map { it.toInt() }.zipWithNext().count { it.first < it.second }
+
+override fun part2() = input.map(String::toInt).windowed(3)
+    .map { it.sum() }.zipWithNext().count { it.first < it.second }
+
+*/
