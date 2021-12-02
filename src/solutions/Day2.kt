6f1bda6e09255d4844f0ba88424f38416aaf1fe5
Wxffel <discordisWxffel#1639@pm.me>
2021-12-02 22:50:47 +0100

    shorter solutions

--- src/solutions/Day2.kt
+++ src/solutions/Day2.kt
@@ -3,19 +3,23 @@ package solutions
 import day
 
 fun main() = day(2) {
-    part1 {
-        var depth = 0
-        var horizontal = 0
 
-        inputLines.forEach { line ->
-            val (cmd, value) = line.split(" ")
-            when (cmd) {
-                "forward" -> horizontal += value.toInt()
-                "up" -> depth -= value.toInt()
-                "down" -> depth += value.toInt()
-            }
+    part1 {
+        inputLines.let { l ->
+            fun f(c: Char) = l.filter { it[0] == c }.sumOf { it.last().digitToInt() }
+            f('f') * (f('d') - f('u'))
         }
-        horizontal * depth
+
+        // or
+
+        // sorted order: d f u
+        val s = inputLines.groupBy({ it[0] }, { it.last().digitToInt() }).toSortedMap().map { it.value.sum() }
+        s[1] * (s[0] - s[2])
+
+        // or
+
+        val g = inputLines.groupBy({ it[0] }, { it.last().digitToInt() })
+        g['f']!!.sum() * (g['d']!!.sum() - g['u']!!.sum())
     }
 
     part2 {
