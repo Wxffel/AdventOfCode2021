a3cbeeaeb43a1453012160f3725f102a60320de5
Wxffel <discordisWxffel#1639@pm.me>
2021-12-02 18:16:41 +0100

    New Day template structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,15 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta3")
+}
+
 tasks {
     sourceSets {
         main {
             java.srcDirs("src")
+            resources.srcDirs("input")
         }
     }
 
