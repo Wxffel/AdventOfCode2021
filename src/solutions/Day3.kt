3c5353d898ad0fcb14392abd2747e375d0792fb4
Wxffel <discordisWxffel#1639@pm.me>
2021-12-03 23:27:21 +0100

    solved Day3

--- src/solutions/Day3.kt
+++ src/solutions/Day3.kt
@@ -1,68 +1,33 @@
 package solutions
 
 import day
+import kotlin.math.pow
 
 fun main() = day(3) {
+    fun mostCommon(binaries: List<String>) = binaries.map { it.withIndex() }.flatten()
+        .groupBy({ it.index }, { it.value })
+        .map { it.value.sorted()[it.value.size / 2] }
+
     part1 {
-        inputLines.asSequence().map { it.mapIndexed { index, c -> index to c } }.flatten()
-            .groupBy({ it.first }, { it.second }).map { it.value.sorted()[it.value.size / 2] }
-            .joinToString("").toInt(2).let { it * it.xor(4095) } // 31 for testcase
+        val xorBy = 2.0.pow(inputLines.first().length).toInt() - 1 // by blue
+        mostCommon(inputLines).joinToString("").toInt(2).let { it * (it xor xorBy) }
     }
 
     part2 {
-
-        var oxygen = "test1"
-        var binariesOxygen = inputLines
-
-        var scrubber = "test2"
-        var binariesScrubber = inputLines
-
-        println("InputLines = $inputLines")
+        var oxygens = inputLines; var scrubbers = inputLines
 
         for (column in 0 until inputLines[0].length) {
+            val mostCommon = mostCommon(oxygens)[column]
+            val leastCommon = if (mostCommon(scrubbers)[column] == '1') '0' else '1'
 
-            if (binariesOxygen.size <= 1) {
-                println("Only one oxygen-binary was left, taking it.")
-                oxygen = binariesOxygen.first()
-            } else {
+            if (oxygens.size > 1)
+                oxygens = oxygens.filter { it[column] == mostCommon }
 
-                val mostCommon = binariesOxygen.asSequence().map { it.mapIndexed { index, c -> index to c } }.flatten()
-                    .groupBy({ it.first }, { it.second })
-                    .map { it.value.sorted()[it.value.size / 2] }[column]
-
-                binariesOxygen = binariesOxygen.filter { binary ->
-                    binary[column] == mostCommon
-                }
-
-                println("binariesOxygen[depth=$column] = $binariesOxygen")
-                oxygen = binariesOxygen.first()
-            }
-
-            if (binariesScrubber.size <= 1) {
-                println("Only one scrubber-binary was left, taking it.")
-                scrubber = binariesScrubber.first()
-            } else {
-
-                val leastCommon = binariesOxygen.asSequence().map { it.mapIndexed { index, c -> index to c } }.flatten()
-                    .groupBy({ it.first }, { it.second })
-                    .map { it.value.sorted()[it.value.size / 2] }[column].let { if (it == '1') '0' else '1' }
-
-
-                binariesScrubber = binariesScrubber.filter { binary ->
-                    binary[column] == leastCommon
-                }
-
-                println("binariesScrubber[depth=$column] = $binariesScrubber")
-                scrubber = binariesScrubber.first()
-            }
-
+            if (scrubbers.size > 1)
+                scrubbers = scrubbers.filter { it[column] == leastCommon }
         }
-        println("oxygen = $oxygen")
-        println("scrubber = $scrubber")
-
-        oxygen.toInt(2) * scrubber.toInt(2)
+        oxygens[0].toInt(2) * scrubbers[0].toInt(2)
     }
-
     expectPart1 = 22 * 9
     expectPart2 = 23 * 10
 }
