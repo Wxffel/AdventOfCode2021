5408b1cac3e999de4a4a7582fec8c0d7d889797a
Wxffel <discordisWxffel#1639@pm.me>
2021-12-03 17:07:43 +0100

    Day3 part 1

--- src/solutions/Day3.kt
+++ src/solutions/Day3.kt
@@ -3,10 +3,10 @@ package solutions
 import day
 
 fun main() = day(3) {
-
-    part1 {  }
-    part2 {  }
-
-    expectPart1
-    expectPart2
+    part1 {
+        inputLines.asSequence().map { it.mapIndexed { index, c -> index to c } }.flatten()
+            .groupBy({ it.first }, { it.second }).map { it.value.sorted()[it.value.size / 2] }
+            .joinToString("").toInt(2).let { it * it.xor(4095) } // 31 for testcase
+    }
+    expectPart1 = 22 * 9
 }
