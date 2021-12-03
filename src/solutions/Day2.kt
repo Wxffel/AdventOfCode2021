8a5cc3b52beb29e4a6eed954c6c99451ea9962c7
Wxffel <discordisWxffel#1639@pm.me>
2021-12-04 00:52:09 +0100

    decided for one solution

--- src/solutions/Day2.kt
+++ src/solutions/Day2.kt
@@ -5,37 +5,27 @@ import day
 fun main() = day(2) {
 
     part1 {
-        inputLines.let { l ->
-            fun f(c: Char) = l.filter { it[0] == c }.sumOf { it.last().digitToInt() }
-            f('f') * (f('d') - f('u'))
-        }
-
-        // or
-
-        // sorted order: d f u
+        // sorted order: down, forward, up
         val s = inputLines.groupBy({ it[0] }, { it.last().digitToInt() }).toSortedMap().map { it.value.sum() }
         s[1] * (s[0] - s[2])
-
-        // or
-
-        val g = inputLines.groupBy({ it[0] }, { it.last().digitToInt() })
-        g['f']!!.sum() * (g['d']!!.sum() - g['u']!!.sum())
     }
 
     part2 {
-        var depth = 0
         var horizontal = 0
+        var depth = 0
         var aim = 0
 
         inputLines.forEach { line ->
-            val (cmd, value) = line.split(" ")
-            when (cmd) {
+            val split = line.split(" ")
+            val value = split[1].toInt()
+
+            when (split[0]) {
                 "forward" -> {
-                    horizontal += value.toInt()
-                    depth += aim * value.toInt()
+                    horizontal += value
+                    depth += aim * value
                 }
-                "up" -> aim -= value.toInt()
-                "down" -> aim += value.toInt()
+                "up" -> aim -= value
+                "down" -> aim += value
             }
         }
         horizontal * depth
