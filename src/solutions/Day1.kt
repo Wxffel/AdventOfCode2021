1ac22b2aa72d7489f386100efd0a2693703b7f86
Wxffel <discordisWxffel#1639@pm.me>
2021-12-04 00:57:01 +0100

    better day1

--- src/solutions/Day1.kt
+++ src/solutions/Day1.kt
@@ -3,22 +3,8 @@ package solutions
 import day
 
 fun main() = day(1) {
-    part1 { inputInts.filterIndexed { index, i -> index != 0 && i > inputInts[index - 1] }.size }
-
-    part2 {
-        inputInts.filterIndexed { index, _ ->
-            index < inputInts.size - 3 &&
-                    inputInts.slice(index + 1..index + 3).sum() > inputInts.slice(index..index + 2).sum()
-        }.size
-    }
-
+    part1 { inputInts.zipWithNext().count { it.first < it.second } }
+    part2 { inputInts.windowed(4).count { it[0] < it[3] } }
     expectPart1 = 7
     expectPart2 = 5
 }
-// Better Solution by BestAuto:
-/*
-override fun part1() = input.map { it.toInt() }.zipWithNext().count { it.first < it.second }
-
-override fun part2() = input.map(String::toInt).windowed(3)
-    .map { it.sum() }.zipWithNext().count { it.first < it.second }
-*/
